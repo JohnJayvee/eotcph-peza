b1f8352bdb6ce7d1c3ec5c8781816ef94e366b44
Ronnie Castro <ronnie.castro@ziaplex.biz>
2021-03-10 10:44:05 +0800

    update

--- app/Laravel/Listeners/SendValidatedEmailListener.php
+++ app/Laravel/Listeners/SendValidatedEmailListener.php
@@ -1,12 +1,11 @@
 <?php 
 namespace App\Laravel\Listeners;
 
-use App\Laravel\Events\SendApprovedEmailReference;
+use App\Laravel\Events\SendValidatedEmailReference;
 
 class SendValidatedEmailListener{
 
 	public function handle(SendValidatedEmailReference $email){
 		$email->job();
-
 	}
 }
