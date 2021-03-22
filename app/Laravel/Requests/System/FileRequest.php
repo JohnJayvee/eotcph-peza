a199566aaf70216817318df06f9a3d15398c6538
Ronnie Castro <ronnie.castro@ziaplex.biz>
2021-03-22 16:42:55 +0800

    update

--- app/Laravel/Requests/System/FileRequest.php
+++ app/Laravel/Requests/System/FileRequest.php
@@ -8,7 +8,7 @@ class FileRequest extends RequestManager{
 	public function rules(){
 		
 		foreach(range(1,count($this->get('name'))) as $index => $value){
-          $rules["document.{$index}"] = "required|mimes:png,jpg,jpeg,pdf,xlsx";
+          $rules["document.{$index}"] = "required|mimes:png,jpg,jpeg,pdf,xlsx|max:8192";
           $rules["name.{$index}"] = "required";
         }
 
@@ -19,6 +19,7 @@ class FileRequest extends RequestManager{
 		return [
 			'required'	=> "Field is required.",
 			'confirmed' => "Password mismatch.",
+			'max' => "Maximum file size to upload is 8MB (8192 KB)."
 		];
 	}
 }
