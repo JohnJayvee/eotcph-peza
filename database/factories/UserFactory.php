fd26cebe65bf8f9f525f67ceaf634ef7557b9d0f
maganasamuel <maganasamuel001@gmail.com>
2021-04-08 01:07:51 +1200

    Set profile picture mock value, active and inactive state for user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,9 @@
 
 use App\Laravel\Models\Department;
 use App\Laravel\Models\User;
+use App\Laravel\Services\ImageUploader;
 use Faker\Generator as Faker;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 
 /*
@@ -19,11 +21,20 @@ use Illuminate\Support\Str;
  */
 
 $factory->define(User::class, function (Faker $faker) {
+    $path = 'uploads/image/users/profile';
+    $image = ImageUploader::upload(UploadedFile::fake()->image('profile-picture.png', 225, 225), 'public/' . $path);
+    $imagePath = Str::replaceFirst('public/' . $path, $path, $image['path']);
+    $imageDirectory = Str::replaceFirst('public/' . $path, $path, $image['directory']);
+
     return [
         'fname' => $faker->firstName,
         'mname' => $faker->lastName,
         'lname' => $faker->lastName,
         'contact_number' => $faker->numerify('+639#########'),
+        'path' => $imagePath,
+        'directory' => $imageDirectory,
+        'filename' => $image['filename'],
+        'status' => 'active',
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -41,6 +52,14 @@ $factory->define(User::class, function (Faker $faker) {
 })->state(User::class, 'processor', function (Faker $faker) {
     return [
         'type' => 'processor',
+    ];
+})->state(User::class, 'active', function () {
+    return [
+        'status' => 'active',
+    ];
+})->state(User::class, 'inactive', function () {
+    return [
+        'status' => 'inactive',
     ];
 })->afterMaking(User::class, function (User $user, Faker $faker) {
     $user->username = Str::slug($user->fname . ' ' . $user->lname);
